e9f9c481c96ec1a1ac98ce5dd0fdeee47a6254b9
syedmhamil <syedmahamil56@gmail.com>
2025-12-26 18:19:23 +0500

    add

--- src/utils/ApiEror.js
+++ src/utils/ApiEror.js
@@ -1,2 +1,25 @@
-// const ApiError extends Error
-//  i  will do tomororomaspes
+
+class ApiError extends Error {
+    stack= ""  
+    constructor(
+        statuscode,
+        message ="Something went Wrong",
+        error = [], 
+        stack = ""
+    ){
+      
+      super(message);
+      this.name = "ValidationError";
+      this.statuscode =statuscode
+      this.data = null 
+      this.error = error
+    
+      if(stack) {
+          this.stack  =stack  
+      }else{
+           Error.captureStackTrace(this,this.constructor)
+      }
+    }
+}
+
+export {ApiError}
