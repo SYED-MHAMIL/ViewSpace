f19ada4787ab10297993ecaf37e89554f1c89e3d
syedmhamil <syedmahamil56@gmail.com>
2025-12-27 20:43:15 +0500

    solve-register

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,11 +4,14 @@ import { ApiError } from "../utils/ApiEror.js"
 
 const  registerUser = (req,res)=>{ 
        const {username,email,fullname,password} =req?.body
-       const isAll =  [username,email,fullname,password].some((field)=> field?.trim == "") 
-
+       console.log({username,email,fullname,password} );
+       
+       const isAll =  [username,email,fullname,password].some((field)=>!field || field?.trim() === "") 
+       console.log
+       ("isall",isAll)
 
         if(isAll){
-            throw new ApiError("404","All field is  required")
+            throw new ApiError(404,"All field is  required")
         }
 }
 
