b0f16858fee41d7c825a776a67516730d8f3d440
syedmhamil <syedmahamil56@gmail.com>
2026-01-23 19:42:34 +0500

    liked

---
+++ src/models/like.models.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+
+const likeVideo  =  Schema({
+     videoId: {
+         type  : Schema.Types.ObjectId,
+         ref: "Video"
+     },
+     liked: {
+        type : Number,
+        default : 0 
+     },
+     unliked:  {
+        type : Number,
+        default : 0 
+     },
+     likedby: {
+           type  : Schema.Types.ObjectId,
+            ref: "User"      
+    },
+     commentId :{
+         type  : Schema.Types.ObjectId,
+         ref: "Comment"
+    },  
+    tweetId : {
+         type  : Schema.Types.ObjectId,
+         ref: "Tweet"
+            
+    }
+    
+})
+
+export const LikeVideo = model("LikeVideo",likeVideo)
